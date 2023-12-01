22708370c540f3088e71d38c22009894fbef5d1c
harm <el.harmo@gmail.com>
2023-12-01 11:28:01 +0100

    part2

--- day_one.pl
+++ day_one.pl
@@ -1,6 +1,5 @@
-
 main :- 
-    open('day1_ex.txt',read,Str),
+    open('day1.txtpl',read,Str),
     read_lines(Str,Y),
     process(X,Y),
     close(Str),
@@ -10,7 +9,7 @@ read_lines(Stream,[]) :-
     at_end_of_stream(Stream).
 
 read_lines(Stream,[X|L]) :-
-    \+ at_end_of_stream(Stream),
+    \+ at_end_of_stream(Stream), %https://en.wikipedia.org/wiki/Prolog#Negation
     read(Stream,X),
     read_lines(Stream,L).
 
@@ -19,33 +18,61 @@ process(0, []).
 process(Res, [H|T]) :- 
     process(X,T),
     nums(Y,H),
-    Res is X + Y.
+    last(Last, Y),
+    first(First, Y),
+    Res is X + (First * 10) + Last.
 
-nums(Res,[H|T]) :-
+last(X,[_H|T]) :- last(X,T).
+last(X,[X]).
+
+first(H,[H|_T]).
+
+% Part two:
+
+nums([1 | Y],[H|T]) :-
+    append(`one`,_Rest,[H|T]),
+    nums(Y, T).
+
+nums([2 | Y],[H|T]) :-
+    append(`two`,_Rest,[H|T]),
+    nums(Y, T).
+
+nums([3 | Y],[H|T]) :-
+    append(`three`,_Rest,[H|T]),
+    nums(Y, T).
+
+nums([4 | Y],[H|T]) :-
+    append(`four`,_Rest,[H|T]),
+    nums(Y, T).
+
+nums([5 | Y],[H|T]) :-
+    append(`five`,_Rest,[H|T]),
+    nums(Y, T).
+
+nums([6 | Y],[H|T]) :-
+    append(`six`,_Rest,[H|T]),
+    nums(Y, T).
+
+nums([7 | Y],[H|T]) :-
+    append(`seven`,_Rest,[H|T]),
+    nums(Y, T).
+
+nums([8 | Y],[H|T]) :-
+    append(`eight`,_Rest,[H|T]),
+    nums(Y, T).
+
+nums([9 | Y],[H|T]) :-
+    append(`nine`,_Rest,[H|T]),
+    nums(Y, T).
+
+% part 1 here
+nums([Num | Y],[H|T]) :-
     H > 47,
     H < 58,
-    nums([X],T),
-    % Res is (H-48)*10 + X.
-    No is H - 48,
-    Res is [No,X].
+    nums(Y, T),
+    Num is H - 48.
 
-nums([Res],[H|T]) :-
-    H > 47,
-    H < 58,
-    nums([],T),
-    Res is H - 48.
-
-
-nums(X,[H|T]) :-
+nums(X,[_H|T]) :-
     nums(X,T).
 
-
-nums([], []).
-
-
-
-sum(Res, [H|T]) :- 
-    sum(X, T), 
-    Res is H + X.
-
-sum(0, []).
+nums([],[]).
---
+++ to_prolog_terms.py
@@ -0,0 +1,14 @@
+import sys
+
+def changed(file):
+    lines = []
+    with open(file, "r") as f:
+        with open(file+"pl", "w") as newf:
+            for line in f:
+                # print(f"`{line.strip()}`.\n")
+                newf.write(f"`{line.strip()}`.\n")
+                # TODO delete last white line join with \n why not in lines
+
+if __name__ == '__main__':
+    file = sys.argv[1]
+    changed(file)
