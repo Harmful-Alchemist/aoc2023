e0588b493cb87ea6145f8a655bffe93f14e1e7bc
harm <el.harmo@gmail.com>
2023-12-15 15:38:24 +0100

    Part 2, hmm only 2 nos per gear

--- day3.pl
+++ day3.pl
@@ -4,7 +4,9 @@ main :-
     % X is 4361, % Ok sure it finds the correct answer but also more.....
     partno([``|Y],X), % add the empty line to have comparison for 1st line
     % close(Str),
-    write(X), nl.
+    gears_no([``|Y],Z),
+    write(X), nl,
+    write(Z), nl.
 
 % Write a quick grammar?
 % Sum : Partnumber Partnumber Sum { $1 + $2 + Sum }
@@ -23,22 +25,101 @@ main :-
 % partno([Line], [Val])
 % partno([],0).
 
+% gear(AsteriskI,[S1,E1,N1],[S2,E2,N2],Val) :- %Adjacent on same line.
+%     AsteriskI is E1 + 1,
+%     AsteriskI is S2 - 1,
+%     Val is N1 * N2.
+
+
+% gears_no([`467..114..`,`...*......`,`..35..633.`],X).
+% Oh bah exactly two adjcent....
+gear(AsteriskI,[S1,E1,N1],[S2,E2,N2],Val) :- 
+    \+ same_number([S1,E1,N1],[S2,E2,N2]),
+    AsteriskI >= S1 - 1,
+    AsteriskI =< E1 + 1,
+    AsteriskI >= S2 - 1,
+    AsteriskI =< E2 + 1,
+    Val is N1 * N2.
+
+same_number([S,E,N],[S,E,N]).
+
+gears(_,[_,_,_],[],0).
+
+gears(I,N1,[S2,E2,N2|_More],Val) :-
+    % write(N1),nl,
+    gear(I,N1,[S2,E2,N2],Val).
+    % write(N),nl,
+    % gears(I,N1,More,Valp), %only if no other 2 number make a gear with an asterisk
+    % Val is Valp + N.
+
+gears(I,N1,[S2,E2,N2|More],Val) :-
+    \+ gear(I,N1,[S2,E2,N2],_),
+    gears(I,N1,More,Val).
+
+gears_more(_,[],_,0).
+
+gears_more(I,[S1,E1,N1|More],N2,Val) :-
+    gears(I,[S1,E1,N1],N2,Valn),
+    gears_more(I,More,N2,Valp),
+    % Valp is Valn,
+    write(Valn),nl,write(Valp),nl,
+    Val is Valp + Valn.
+
+% gears_more(I,[S1,E1,N1|More],N2,Val) :-
+%     \+ gears(I,[S1,E1,N1],N2,_),
+%     gears_more(I,More,N2,Val).
+
+
+gears_all([],_,0).
+gears_all([I|Is],Ns,Val) :-
+    gears_more(I,Ns,Ns,Valn),
+    gears_all(Is,Ns,Valp),
+    Val is Valn/2 + Valp. % We get them from both numbers...
+
+gears_no([],0).
+
+gears_no([L1,L2,L3|More],Val) :-
+    % \+ empty(More), % TODO when empty! Altho...
+    info(L1,_,_,N1,_,[]),
+    info(L2,_,_,N2,G2,[]),
+    info(L3,_,_,N3,_,[]),
+    combi(N1,N2,N3,Ns),
+    gears_all(G2,Ns,Valn),
+    gears_no([L2,L3|More],Valp),
+    Val is Valn + Valp.
+
+gears_no([L1,L2],Val) :-
+    info(L1,_,_,N1,G1,[]),
+    info(L2,_,_,N2,G2,[]),
+    append(N1,N2,Ns),
+    gears_all(G1,Ns,Val1),
+    gears_all(G2,Ns,Val2),
+    Val is Val1 + Val2.
+
+gears_no([L],Val) :-
+    info(L,_,_,N,G,[]),
+    gears_all(G,N,Val).
+
+combi(L1,L2,L3,CL) :-
+    append(L1,L2,N),
+    append(N,L3,CL).
+
 partno([L],Val) :-
-    info(L,_,S,N,[]),
+    info(L,_,S,N,_,[]),
     parts_adjacent(S,N,Val,_).
 
 partno([L1,L2],Val) :-
-    info(L1,_,S1,N1,[]),
-    info(L2,_,S2,N2,[]),
+    info(L1,_,S1,N1,_,[]),
+    info(L2,_,S2,N2,_,[]),
     parts_adjacent(S1,N1,Val1a,N1_1),
     parts_diag(S2,N1_1,Val1d,_),
     parts_adjacent(S2,N2,Val2,_),
     Val is Val2 + Val1a + Val1d.
 
 partno([L1,L2,L3|More],Val) :-
-    info(L1,_,S1,_,[]),
-    info(L2,_,S2,N2,[]),
-    info(L3,_,S3,_,[]),
+    info(L1,_,S1,_,_,[]),
+    info(L2,_,S2,N2,_,[]),
+    info(L3,_,S3,_,_,[]),
     parts_adjacent(S2,N2,A,Nn),
     parts_diag(S1,Nn,B,Nnn),
     parts_diag(S3,Nnn,C,_),
@@ -47,9 +128,9 @@ partno([L1,L2,L3|More],Val) :-
     Val is Valp + A + B + C.
 
 partno([L1,L2,L3|More],Val) :-
-    info(L1,_,S1,_,[]),
-    info(L2,_,S2,N2,[]),
-    info(L3,_,S3,N3,[]),
+    info(L1,_,S1,_,_,[]),
+    info(L2,_,S2,N2,_,[]),
+    info(L3,_,S3,N3,_,[]),
     parts_adjacent(S2,N2,A,Nn),
     parts_diag(S1,Nn,B,Nnn),
     parts_diag(S3,Nnn,C,_),
@@ -105,51 +186,59 @@ contains_bound(Min,Max,[_|Symbols]) :-
     contains_bound(Min,Max,Symbols).
 
 % info(Line,CurrentIndex,Symbols,Numbers,Remaining).
-info([],0,[],[],[]).
+info([],0,[],[],[],[]).
 
-info([S|Out],I,[I|A],B,[]) :-
+info([S|Out],I,[I|A],B,C,[]) :-
     symbol(S),
-    info(Out,Ii,A,B,[]),
+    \+ asterisk(S),
+    info(Out,Ii,A,B,C,[]),
     I is Ii + 1.
 
-info([N1,N2,N3|Out],Iend,[Istart,Imiddle,Iend|A],[Istart,Iend,Val|B],[]) :-
+info([S|Out],I,[I|A],B,[I|C],[]) :-
+    symbol(S),
+    asterisk(S),
+    info(Out,Ii,A,B,C,[]),
+    I is Ii + 1.
+
+info([N1,N2,N3|Out],Iend,[Istart,Imiddle,Iend|A],[Istart,Iend,Val|B],C,[]) :-
     number([N1,N2,N3],Val,[]),
-    info(Out,Ii,A,B,[]),
+    info(Out,Ii,A,B,C,[]),
     Istart is Ii + 1,
     Imiddle is Ii +2,
     Iend is Ii + 3.
 
-info([N1,N2],Iend,[Istart,Iend|A],[Istart,Iend,Val|B],[]) :-
+info([N1,N2],Iend,[Istart,Iend|A],[Istart,Iend,Val|B],C,[]) :-
     number([N1,N2],Val,[]),
-    info([],Ii,A,B,[]),
+    info([],Ii,A,B,C,[]),
     Istart is Ii + 1,
     Iend is Ii + 2.
 
-info([N1,N2,N3|Out],Iend,[Istart,Iend|A],[Istart,Iend,Val|B],[]) :-
+info([N1,N2,N3|Out],Iend,[Istart,Iend|A],[Istart,Iend,Val|B],C,[]) :-
     \+ number([N1,N2,N3],Val,[]),
     number([N1,N2],Val,[]),
-    info([N3|Out],Ii,A,B,[]),
+    info([N3|Out],Ii,A,B,C,[]),
     Istart is Ii + 1,
     Iend is Ii + 2.
 
-info([N],I,[I|A],[I,I,Val|B],[]) :-
+info([N],I,[I|A],[I,I,Val|B],C,[]) :-
     number([N],Val,[]),
-    info([],Ii,A,B,[]),
+    info([],Ii,A,B,C,[]),
     I is Ii + 1.
 
-info([N, N2|Out],I,[I|A],[I,I,Val|B],[]) :-
+info([N, N2|Out],I,[I|A],[I,I,Val|B],C,[]) :-
     \+ number([N,N2],Val,[]),
     number([N],Val,[]),
-    info([N2|Out],Ii,A,B,[]),
+    info([N2|Out],Ii,A,B,C,[]),
     I is Ii + 1.
 
-info([S|Out],I,A,B,[]) :-
+info([S|Out],I,A,B,C,[]) :-
     dot(S),
-    info(Out,Ii,A,B,[]),
+    info(Out,Ii,A,B,C,[]),
     I is Ii + 1.
 
 
-
+asterisk(S) :-
+    S is 42.
 % 46 is ascii dot
 dot(S) :-
     S is 46.
