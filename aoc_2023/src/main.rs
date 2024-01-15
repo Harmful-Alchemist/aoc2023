33f5b353692a844846bd8f88c4c5dfcd3c40f4c4
harm <el.harmo@gmail.com>
2024-01-15 13:22:17 +0100

    ok then

--- aoc_2023/src/main.rs
+++ aoc_2023/src/main.rs
@@ -1,6 +1,12 @@
-use std::fs;
+use std::{
+    fs,
+    ops::Range,
+    sync::mpsc::{self, Sender},
+    thread,
+};
 
 fn main() {
+    // dbg!(u64::MAX);
     let five_1_ex = day5_1("./../day5_ex.txt");
     println!("{five_1_ex}");
     let five_1_ex = day5_1("./../day5.txt");
@@ -40,11 +46,90 @@ fn day5_2(path: &str) -> u64 {
         .filter_map(|w| w.parse::<u64>().ok())
         .collect::<Vec<u64>>();
 
-    let seeds_with_range_iter = seeds.chunks(2).flat_map(|s| s[0]..(s[0] + s[1]));
+    let _ = lines.next();
+    let _ = lines.next();
 
-    let _ = lines.next();
-    let _ = lines.next();
-    lol(lines, seeds_with_range_iter)
+    let mut maps: [Vec<Mapping>; 7] = [
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+    ];
+    let mut current = 0;
+
+    for line in lines {
+        if line == "" {
+            continue;
+        }
+
+        let mut x = line.split(" ").filter_map(|f| f.parse::<u64>().ok());
+        let y = x.next();
+        match y {
+            Some(z) => maps[current].push(Mapping::from([z, x.next().unwrap(), x.next().unwrap()])),
+            None => {
+                current += 1;
+                continue;
+            }
+        }
+    }
+
+    let threads = seeds.len();
+    // let threads = 16;
+
+    let mut calcs: Vec<Sender<(Range<u64>, Option<Sender<u64>>)>> = Vec::with_capacity(threads);
+
+    for i in 0..threads {
+        let (tx, rx) = mpsc::channel();
+        calcs.push(tx);
+        let maps = maps.clone();
+        let builder = thread::Builder::new().name(i.to_string());
+
+        builder
+            .spawn(move || {
+                let mut my_min = u64::MAX;
+                loop {
+                    let rec = rx.recv();
+                    match rec {
+                        Ok((seeds, None)) => {
+                            my_min = seeds
+                                .map(|seed| {
+                                    maps.iter().fold(seed, |acc, e| {
+                                        e.iter().find_map(|m| m.check(acc)).or(Some(acc)).unwrap()
+                                    })
+                                })
+                                .min()
+                                .unwrap();
+                        }
+                        Ok((_, Some(return_tx))) => {
+                            return_tx.send(my_min).unwrap();
+                            break;
+                        }
+                        _ => panic!(),
+                    }
+                }
+            })
+            .unwrap();
+    }
+
+    seeds
+        .chunks(2)
+        .map(|s| s[0]..(s[0] + s[1]))
+        .enumerate()
+        .for_each(|(ix, range)| calcs[ix % threads].send((range, None)).unwrap());
+
+    calcs
+        .into_iter()
+        .map(|c| {
+            let (tx, rx) = mpsc::channel();
+            c.send((0..0, Some(tx))).unwrap();
+            rx.recv().unwrap()
+        })
+        .min()
+        .unwrap()
+
 }
 
 fn lol(lines: std::str::Lines<'_>, seeds: impl Iterator<Item = u64>) -> u64 {
@@ -85,6 +170,7 @@ fn lol(lines: std::str::Lines<'_>, seeds: impl Iterator<Item = u64>) -> u64 {
         .unwrap()
 }
 
+#[derive(Clone)]
 struct Mapping {
     dest_start: u64,
     source_start: u64,
